c7fd94af2d8cd0ce8eef287e944c70c5fedfecd5
Rasel Sharif <raselsharif9@gmail.com>
2023-08-26 20:58:53 +0600

    added fetch more

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,17 @@
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
-fetch(url)
-    .then(response => response.json())
-    .then(json => console.log(json))
+
+function loadData(){
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then (data => displayData(data));
+}
+
+function displayData(data){
+    console.log(data);
+}
