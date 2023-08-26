e52d78e282f7209735c0a78789d5478aed64f619
Rasel Sharif <raselsharif9@gmail.com>
2023-08-26 22:17:00 +0600

    added fetch load data in html

--- js/fetch.js
+++ js/fetch.js
@@ -1,17 +1,27 @@
 
-function loadData(){
+function loadData() {
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
     fetch(url)
         .then(response => response.json())
         .then(json => console.log(json))
 }
 
-function loadUsers(){
+function loadUsers() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then (data => displayData(data));
+        .then(res => res.json())
+        .then(data => displayData(data));
 }
 
-function displayData(data){
-    console.log(data);
+function displayData(data) {
+    const ul = document.getElementById('user-list');
+    ul.classList.add('py-5')
+    for (const user of data) {
+        const li = document.createElement('li');
+        const count = ul.childElementCount;
+        li.classList.add('text-2xl', 'py-2')
+        li.innerText = `Serial no: ${count + 1},
+        User Name: ${user.name} ,
+        Email: ${user.email}`;
+        ul.appendChild(li);
+    }
 }
